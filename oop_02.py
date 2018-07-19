03cfb6fce6db0a125a254d9af1fe8f484ee4ba23
Harry <shzhouhp@gmail.com>
2018-07-19 20:52:30 +0800

    modify the oop_02.py

--- oop_02.py
+++ oop_02.py
@@ -3,7 +3,7 @@ class Student(object):
     count=0
 
     def __init__(self,name):       #要注意到，每创建一个实例，__init__()是要执行的
-        Student.count+=1
+        Student.count+=1           #注意一定要用Student.count而不是count,不完全理解
         self.name=name
     
     def print_name(self):
